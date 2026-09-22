4decc5063be68b23ba86ef779697c00aa6fb799c
citrusPokus <clypeummail@duck.com>
2026-09-22 12:51:35 +0200

    Create __init__.py

---
+++ index/__init__.py
@@ -0,0 +1,15 @@
+from .database import (
+    AssetDatabase,
+    AssetDetails,
+    IndexedAsset,
+    TagRecord,
+    ThumbnailRecord,
+)
+
+__all__ = [
+    "AssetDatabase",
+    "AssetDetails",
+    "IndexedAsset",
+    "TagRecord",
+    "ThumbnailRecord",
+]
